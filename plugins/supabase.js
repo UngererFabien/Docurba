b1c0abc3d4074222df2ca704cd9e5e7d6765fa95
Fabien Ungerer <fabien.ungerer@gmail.com>
2025-01-30 15:50:45 +0100

    supabase auth token in cookies

--- plugins/supabase.js
+++ plugins/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 // import { PG_DEV_CONFIG, PG_PROD_CONFIG } from '@/database/pg_secret_config.json'
 
 export default (_, inject) => {
@@ -6,7 +6,7 @@ export default (_, inject) => {
   const supabaseUrl = 'https://supabase.docurba.beta.gouv.fr'
   const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDk3NTU3NiwiZXhwIjoxOTUwNTUxNTc2fQ.zvgWeMG3sKSwBKv8uGm5uy82cEyMyEMivQvNIDFhBkA'
 
-  inject('supabase', createClient(supabaseUrl, supabaseAnonKey))
+  inject('supabase', createBrowserClient(supabaseUrl, supabaseAnonKey))
   // const DB_CONFIG = PG_DEV_CONFIG
   // console.log(`Running on ${DB_CONFIG.env === 'dev' ? 'Dev' : 'Prod'} database instance.`)
   // console.log('Need run with PG_PROD_CONFIG for deploy: ', PG_PROD_CONFIG)
