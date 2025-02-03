1b25d911ecbb2e22641cb5fa35f05e830aeaaa51
Anthony Ricaud <anthony@ricaud.me>
2025-02-03 08:53:18 +0000

    Ne recrée plus un index sans raison apparente

--- nuxt/daily_dump/steps/sql/status_handler/3-set_all_procedures_status.sql
+++ nuxt/daily_dump/steps/sql/status_handler/3-set_all_procedures_status.sql
@@ -1,10 +1,4 @@
 set statement_timeout to 600000;
-DROP INDEX IF EXISTS idx_doc_frise_events_procedure_id;
-DROP INDEX IF EXISTS idx_doc_frise_events_procedure_id_date_iso;
-
-CREATE INDEX idx_doc_frise_events_procedure_id ON doc_frise_events (procedure_id);
-CREATE INDEX idx_doc_frise_events_procedure_id_date_iso ON doc_frise_events (procedure_id, date_iso DESC);
-
 
 CREATE OR REPLACE FUNCTION one_shot_events()
 RETURNS void AS $$
